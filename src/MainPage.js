02cef7623e5dfdf2f7c05e6ebb562c1093790c1f
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2021-11-22 00:31:49 +0200

    adding components step2

---
+++ src/MainPage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Book from './Book'
+
+class MainPage extends Component{
+
+render(){
+
+    return(
+
+        <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                  <Book/>
+                  </li>
+                  <li>
+                  <Book/>
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book/>
+                  </li>
+                  <li>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
+                      <div className="book-authors">J.K. Rowling</div>
+                    </div>
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                   <Book/>
+                  </li>
+                  <li>
+                  <Book/>
+                  </li>
+                  <li>
+                   <Book/>
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+        </div>
+      </div>
+    );
+}
+
+
+
+}
+
+export default MainPage;
