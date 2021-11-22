7d33376e24dd78a805a5e2d89edf7ba95848a7b2
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2021-11-22 02:06:41 +0200

    adding State

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,7 +4,7 @@ import Book from './Book'
 class MainPage extends Component{
 
 render(){
-
+console.log(this.props.books);
     return(
 
         <div className="list-books">
@@ -17,12 +17,13 @@ render(){
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                  <Book/>
-                  </li>
-                  <li>
-                  <Book/>
-                  </li>
+                {
+                    this.props.books.filter(book=>book.shelf==='currentlyReading').map(book=> <li key={book.id}>
+                                                                                                    <Book />
+                                                                                                </li>)
+                }
+                  
+                
                 </ol>
               </div>
             </div>
@@ -30,27 +31,11 @@ render(){
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book/>
-                  </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
-                      <div className="book-authors">J.K. Rowling</div>
-                    </div>
-                  </li>
+                {
+                    this.props.books.filter(book=>book.shelf==='wantToRead').map(book=> <li key={book.id}>
+                                                                                                    <Book />
+                                                                                                </li>)
+                }
                 </ol>
               </div>
             </div>
@@ -58,15 +43,11 @@ render(){
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                   <Book/>
-                  </li>
-                  <li>
-                  <Book/>
-                  </li>
-                  <li>
-                   <Book/>
-                  </li>
+                {
+                    this.props.books.filter(book=>book.shelf==='read').map(book=> <li key={book.id}>
+                                                                                                    <Book />
+                                                                                                </li>)
+                }
                 </ol>
               </div>
             </div>
